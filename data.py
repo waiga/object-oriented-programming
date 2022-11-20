18e8a918957fb79bd5a932ad95c7eff71a752728
waiga <hyang9387@gmail.com>
2022-11-20 16:53:04 +0900

    Initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+{"text": "300 + 13 = 313", "answer": "True"},
+{"text": "25 * 4 = 120", "answer": "False"},
+{"text": "35 + 132 = 167", "answer": "True"},
+{"text": "43 + 57 = 100.", "answer": "True"},
+{"text": "10 - 2 = 8", "answer": "True"},
+{"text": "123 + 456 = 123456", "answer": "False"},
+{"text": "60^2 = 120", "answer": "True"},
+{"text": "77 * 2 = 144", "answer": "False"},
+{"text": "5 + 3 * 3 = 14", "answer": "True"},
+{"text": "7 - 3 * 2 = 1", "answer": "True"},
+{"text": "e = 3.14314", "answer": "False"},
+{"text": "6x + 10 = 22, x = 2", "answer": "True"}
+]
